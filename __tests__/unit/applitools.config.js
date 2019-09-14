9a6927e91815014c20847d330734959a1632c288
Sai <saikrishna321@yahoo.com>
2019-09-14 15:57:53 +0100

    prettier write on commit

--- __tests__/unit/applitools.config.js
+++ __tests__/unit/applitools.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    apiKey: 'taiko-appli',
-    isDisabled: false,
-    showLogs: true,
-    someKey: 'someValue',
-  };
+  apiKey: 'taiko-appli',
+  isDisabled: false,
+  showLogs: true,
+  someKey: 'someValue',
+};
--- __tests__/unit/initDefaultConfig.spec.js
+++ __tests__/unit/initDefaultConfig.spec.js
@@ -2,20 +2,18 @@ const path = require('path');
 const initDefaultConfig = require('../../src/initDefaultConfig');
 
 test('Init Default Config', async () => {
-    const configPath = path.resolve(__dirname, './applitools.config.js');
-    process.env.APPLITOOLS_API_KEY = 'API-KEY';
-    process.env.APPLITOOLS_TAP_DIR_PATH = 'SOME PATH';
-    const config = initDefaultConfig(configPath);
-    expect(config).toEqual(
-        {
-            agentId: `eyes-testcafe/${packageVersion}`,
-            apiKey: 'OVERIDEN!!!',
-            concurrency: 1,
-            failTestcafeOnDiff: true,
-            isDisabled: false,
-            showLogs: true,
-            someKey: 'someValue',
-            tapDirPath: 'SOME PATH',
-          }
-    )
+  const configPath = path.resolve(__dirname, './applitools.config.js');
+  process.env.APPLITOOLS_API_KEY = 'API-KEY';
+  process.env.APPLITOOLS_TAP_DIR_PATH = 'SOME PATH';
+  const config = initDefaultConfig(configPath);
+  expect(config).toEqual({
+    agentId: `eyes-testcafe/${packageVersion}`,
+    apiKey: 'OVERIDEN!!!',
+    concurrency: 1,
+    failTestcafeOnDiff: true,
+    isDisabled: false,
+    showLogs: true,
+    someKey: 'someValue',
+    tapDirPath: 'SOME PATH',
   });
+});
