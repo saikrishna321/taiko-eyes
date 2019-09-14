fd9a1857d3361c4fc94cad1e8a7371444cab2c29
Sai <saikrishna321@yahoo.com>
2019-09-14 16:08:38 +0100

    added unit test for inticonfig

--- __tests__/unit/initDefaultConfig.spec.js
+++ __tests__/unit/initDefaultConfig.spec.js
@@ -2,15 +2,14 @@ const path = require('path');
 const initDefaultConfig = require('../../src/initDefaultConfig');
 
 test('Init Default Config', async () => {
-  const configPath = path.resolve(__dirname, './applitools.config.js');
+  const configPath = path.resolve(__dirname, '../../testSeed/applitools.config.js');
   process.env.APPLITOOLS_API_KEY = 'API-KEY';
   process.env.APPLITOOLS_TAP_DIR_PATH = 'SOME PATH';
   const config = initDefaultConfig(configPath);
   expect(config).toEqual({
-    agentId: `eyes-testcafe/${packageVersion}`,
-    apiKey: 'OVERIDEN!!!',
+    apiKey: 'API-KEY',
     concurrency: 1,
-    failTestcafeOnDiff: true,
+    failTaikoOnDiff: true,
     isDisabled: false,
     showLogs: true,
     someKey: 'someValue',
